8fd518603f68e85a5e6c5efe57c2b3554378d714
Josh Adkins <adkinsjd@umich.edu>
2019-04-30 21:52:26 -0700

    add apn and filtering to build script

--- firmware/particle/build_all.py
+++ firmware/particle/build_all.py
@@ -2,6 +2,13 @@
 import sys
 import yaml
 import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(description = 'Deploy particle firmware')
+parser.add_argument('-n','--name',type=str,required=False)
+
+args = parser.parse_args()
+
 
 
 with open("build_settings.yaml", 'r') as stream:
@@ -9,10 +16,16 @@ with open("build_settings.yaml", 'r') as stream:
 
     #now for each build in the build list
     for build in data['build']:
+
+        if args.name != None:
+            if build['name'] != args.name:
+                continue
+
         #write the product_id header file with the two defines
         header = open(build['folder'] + '/src/product_id.h', 'w')
         header.write("#define PRODUCT " + str(build['product_id']) + '\n')
         header.write('#define DEPLOYMENT "' + build['name'] + '"\n')
+        header.write('#define AERIS_APN "' + build['apn'] + '"\n')
         header.close()
 
         #build the firmware
