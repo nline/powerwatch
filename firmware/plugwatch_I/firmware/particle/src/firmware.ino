0567c793a1fb4e9843c0a360a65e4377b2695137
Joshua Adkuins <adkins@berkeley.edu>
2020-02-12 03:17:59 +0000

    bump version number

--- firmware/plugwatch_I/firmware/particle/src/firmware.ino
+++ firmware/plugwatch_I/firmware/particle/src/firmware.ino
@@ -40,8 +40,8 @@ int product_id = 10804;
 PRODUCT_ID(10804);
 #endif
 
-int version_int = 205; 
-PRODUCT_VERSION(205);
+int version_int = 206; 
+PRODUCT_VERSION(206);
 
 SYSTEM_THREAD(ENABLED);
 STARTUP(System.enableFeature(FEATURE_RESET_INFO));
